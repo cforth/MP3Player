8700620ae04de65c85481c13e8866b1961de1a8a
chaifei <cforth@163.com>
2020-10-02 11:02:06 +0800

    完善对lrc格式的支持

--- LyricUtils.py
+++ LyricUtils.py
@@ -2,7 +2,7 @@ import re
 import os
 
 
-# 将歌词时间戳字符串转为秒数，字符串格式为 01:56.43
+# 将歌词时间戳字符串转为秒数，歌词文件要符合lrc格式
 def get_lyric_seconds(lyric_time_str):
     fen = int(lyric_time_str[0:2])
     secs = int(lyric_time_str[3:5])
@@ -17,12 +17,19 @@ def read_lyric(lyric_path):
     lyric_str_dirt = {}
     with open(lyric_path, "r", encoding="utf-8") as f:
         lyric_list = f.readlines()
-    for l in lyric_list:
-        lyric_str = l[10:].replace("\n", "")
-        if re.match(r'\[\d\d:\d\d\.\d\d]*', l):
+    for lyric_line in lyric_list:
+        lyric_line = lyric_line.replace("\r", "")
+        lyric_line = lyric_line.replace("\n", "")
+        times = re.findall(r'(\d{2}:\d{2}[:|.]\d{2})+', lyric_line)
+        times = re.findall(r'(\d{2}:\d{2})+', lyric_line) if not times else times
+        if times:
+            lyric_str = lyric_line[lyric_line.rindex("]")+1:]
             if lyric_str:
-                lyric_str_dirt[get_lyric_seconds(l[1:9])] = lyric_str
+                for t in times:
+                    lyric_str_dirt[get_lyric_seconds(t)] = lyric_str
+
     return lyric_str_dirt
 
 
+# print(read_lyric("./lyric/许冠杰 - 浪子心声.lrc"))
 # print(read_lyric("./lyric/展展与罗罗 - 沙漠骆驼.lrc"))
